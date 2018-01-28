b19783f697b670c042ea767370de34a1931333e3
EmmaHon <emmaho@metropolia.fi>
2018-01-29 01:09:07 +0200

    some changes to the task

--- src/app/list-media/list-media.component.ts
+++ src/app/list-media/list-media.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MediaService} from '../services/media.service';
 import index from '@angular/cli/lib/cli';
+import {DigitransitService} from "../services/digitransit.service";
 
 @Component({
   selector: 'app-list-media',
@@ -12,20 +13,26 @@ export class ListMediaComponent implements OnInit {
   printThis: String;
   mediaArray: any;
 
-  constructor(public mediaService: MediaService) { }
+  stopArray: any;
+
+  constructor(public mediaService: MediaService, private digitransitService: DigitransitService) { }
 
   ngOnInit() {
     this.printThis = this.mediaService.test;
-    this.mediaService.getAllMedia().subscribe(data=> {
+    this.mediaService.getAllMedia().subscribe(data => {
       console.log(data);
       this.mediaArray = data;
 
-      this.mediaArray.map(media =>{
+      this.mediaArray.map(media => {
         const temp = media.filename.split('.');
         const thumbName = temp[0] + '-tn320.png';
         media.thumbnail = thumbName;
       });
       console.log(this.mediaArray);
+    });
+    this.digitransitService.getRoutes('Urheilutie 2').subscribe(response => {
+      console.log(response['data'].stops);
+      this.stopArray = response['data'].stops;
     });
   }
 }
